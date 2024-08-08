a12db922de50c9a49bcf0a6ca14056de76a467a1
Tayfun Sener <tayfunsener2@gmail.com>
2024-08-08 17:23:10 +0300

    Initial dashboard ui

--- health-app/ContentView.swift
+++ health-app/ContentView.swift
@@ -9,16 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    VStack {
+                        HStack{
+                            VStack (alignment: .leading){
+                                Label("Steps", image: "figure.walk.motion")
+                                    .font(.title3.bold())
+                                    .foregroundColor(.mint)
+                                Text("Avg:10K Steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.mint)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    .padding(.bottom, 30)
+                    
+                    VStack(alignment: .leading) {
+                        
+                            VStack (alignment: .leading){
+                                Label("Averages", image: "calendar")
+                                    .font(.title3.bold())
+                                    .foregroundColor(.mint)
+                                Text("Last 28 Days")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                        
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 250)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                }
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 
+
 #Preview {
     ContentView()
+        
 }
