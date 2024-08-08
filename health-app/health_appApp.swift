439e3150d4a48ea64f0b4cfd40b74f05c10a3fdf
Tayfun Sener <tayfunsener2@gmail.com>
2024-08-08 13:30:53 +0300

    created xcode project
    DSStore ignored

---
+++ health-app/health_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_appApp.swift
+//  health-app
+//
+//  Created by Tayfun Sener on 8.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct health_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
