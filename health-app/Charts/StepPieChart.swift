5e8842e4cc2c44845f0dc61b6e2229e4f1ff2551
Tayfun Sener <tayfunsener2@gmail.com>
2024-08-11 23:58:41 +0300

    update stepPieChart

--- health-app/Charts/StepPieChart.swift
+++ health-app/Charts/StepPieChart.swift
@@ -10,6 +10,18 @@ import Charts
 
 struct StepPieChart: View {
     
+    @State private var rawSelectedChartValue: Double? = 0
+    
+    var selectedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+        var total = 0.0
+        
+        return chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+    }
+    
     var chartData: [WeekdayChartData]
     
     var body: some View {
@@ -28,15 +40,37 @@ struct StepPieChart: View {
                 ForEach(chartData) { weekday in
                     SectorMark(angle: .value("average steps", weekday.value),
                                innerRadius: .ratio(0.7),
+                               outerRadius: selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
                                angularInset: 3
                     )
                     .foregroundStyle(.mint.gradient)
                     .cornerRadius(4)
-                    
+                    .opacity(selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1.0 : 0.4)
                         
                 }
             }
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        let frame = geo[plotFrame]
+                        if let selectedWeekday {
+                            VStack {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x:frame.midX, y:frame.midY)
+                        }
+                    }
+                    
+                }
+                
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
