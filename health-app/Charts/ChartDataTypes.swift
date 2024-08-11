a2e5f4f7a6f218a6a1011eb7bed0eafa89007959
Tayfun Sener <tayfunsener2@gmail.com>
2024-08-11 22:39:03 +0300

    average step calculation

---
+++ health-app/Charts/ChartDataTypes.swift
@@ -0,0 +1,14 @@
+//
+//  ChartDataTypes.swift
+//  health-app
+//
+//  Created by Tayfun Sener on 11.08.2024.
+//
+
+import Foundation
+
+struct WeekdayChartData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let value: Double
+}
