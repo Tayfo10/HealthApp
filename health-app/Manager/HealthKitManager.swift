cdbb86e80cec8a6721a2ce1fae603bc06710f90f
Tayfun Sener <tayfunsener2@gmail.com>
2024-08-15 15:15:23 +0300

    Update HealthKitManager

--- health-app/Manager/HealthKitManager.swift
+++ health-app/Manager/HealthKitManager.swift
@@ -205,28 +205,4 @@ import Observation
         let startDate = calendar.date(byAdding: .day, value: -daysBack, to: endDate)!
         return .init(start: startDate, end: endDate)
     }
-    
-    
-    /// Injects simulator data to HealthKit
-    func addSimulatorData() async {
-        
-        var mockSamples: [HKQuantitySample] = []
-        for i in 0..<28 {
-            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
-            let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
-            
-            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
-            let weightQuantity = HKQuantity(unit: .pound(), doubleValue: .random(in: 160 + Double(i/3)...165 + Double(i/3)))
-            let caloriesQuantity = HKQuantity(unit: .largeCalorie(), doubleValue: .random(in: 100...900))
-            
-            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
-            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuantity, start: startDate, end: endDate)
-            let caloriesSample = HKQuantitySample(type: HKQuantityType(.activeEnergyBurned), quantity: caloriesQuantity, start: startDate, end: endDate)
-            
-            mockSamples.append(stepSample)
-            mockSamples.append(weightSample)
-            mockSamples.append(caloriesSample)
-        }
-        try! await store.save(mockSamples)
-    }
 }
